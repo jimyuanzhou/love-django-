5e5344ac6a94c7d1b50581f2467f87277cd8c1c8
jimyuanzhou <jimyuanzhou@gmail.com>
2021-09-21 11:47:52 -0400

    store and carts functionality

--- accounts/models.py
+++ accounts/models.py
@@ -57,7 +57,7 @@ class Account(AbstractBaseUser):
 
     objects = MyAccountManager()
 
-    def _str_ (self):
+    def __str__ (self):
         return self.email
 
     def has_perm(self, perm, obj=None):
